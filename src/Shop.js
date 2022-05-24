b6296c143a0638295cdeba42ca20ccacdab75faf
SUMANTA5 <sumanta111195@gmail.com>
2022-05-24 09:53:55 +0530

    nav

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div> 
+        <h2> Bay Adidas shoes 50 Rs</h2>
+        <button className="btn-primary mx-2">-</button>
+        add to cart
+        <button className="btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
